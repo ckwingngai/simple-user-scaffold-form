26f9811ff611d57bb34e8ca4867a5be2353fa4fa
raymond <ckwingngai@gmail.com>
2016-09-06 11:40:21 +0800

    init commit form with image upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :microposts
   validates :name, presence: true
   validates :email, presence: true
+
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
